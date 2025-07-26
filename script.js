019680114420a784cf224164af2a3825e4b7c315
Malik Ubaid <malikubaidawan14@gmail.com>
2025-07-26 14:00:24 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function addTask() {
+  let input = document.getElementById("task-input");
+  let taskText = input.value.trim();
+  if (taskText !== "") {
+    let li = document.createElement("li");
+    li.textContent = taskText;
+    document.getElementById("task-list").appendChild(li);
+    input.value = "";
+  }
+}
